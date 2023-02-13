5b2fd5a12d9ad4cf42f8bafeb01f7fa64ca69e1e
Manikanta Reddy <manikantareddy9848@gmail.com>
2023-02-13 20:20:51 +0530

    task

--- Question3.py
+++ Question3.py
@@ -1,11 +1,19 @@
 def highest_frequency(arr):
-  frequency = {}
-  for i in arr:
-    if i in frequency:
-      frequency[i] += 1
-    else:
-      frequency[i] = 1
-  return max(frequency, key=frequency.get)
+  
+    frequency_dict = {}
+    
+    for num in arr:
+        
+        if num not in frequency_dict:
+            frequency_dict[num] = 1
+        
+        else:
+            frequency_dict[num] += 1
+   
+    max_frequency = max(frequency_dict.values())
+    for key, value in frequency_dict.items():
+        if value == max_frequency:
+            return key
 
-array = [1,2,4,3,2,4,2,5,7,2]
-print(highest_frequency(array))
+arr = [1, 2, 4, 3, 2, 4, 2, 5, 7, 2]
+print(highest_frequency(arr)) 
